2c003de610b6bb01b636fd16ee6ab7eecf316d2d
Ridhima <22b01a12b6@svecw.edu.in>
2024-09-04 18:59:05 +0530

    built signin/signup forms

---
+++ src/components/Login.js
@@ -0,0 +1,52 @@
+import Header from "./Header";
+import { useState } from "react";
+const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const toggleSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/dae1f45f-c2c5-4a62-8d58-6e1b0c6b2d8e/6d1fb8a4-5844-42a4-9b01-1c6c128acf19/IN-en-20240827-TRIFECTA-perspective_WEB_c292a608-cdc6-4686-8dc8-405bfcf753af_large.jpg" />
+      </div>
+      <div className=" w-3/12 bg-black absolute  center my-40 mx-auto right-0 left-0 p-8 bg-opacity-80 rounded-md py-14">
+        <h1 className=" text-white text-2xl font-bold">
+          {isSignIn ? " Sign In" : "Sign Up"}
+        </h1>
+        <form>
+          {!isSignIn && (
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-full h-12 p-6 m-2 text-white bg-transparent border border-gray-600 ml-0 rounded-md mt-4 bg-slate-950"
+            />
+          )}
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="w-full h-12 p-6 m-2 text-white bg-transparent border border-gray-600 ml-0 rounded-md mt-4 bg-slate-950"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full h-12 p-6  bg-transparent border border-gray-600 m-4 ml-0 rounded-md bg-slate-950"
+          />
+          <input
+            type="submit"
+            className=" bg-red-600 text-white font-semibold w-full p-3 m-4 ml-0 rounded-md"
+          />
+        </form>
+        <p className="  text-white mt-3">
+          {isSignIn ? "New to Netfix?" : "Already a user?"}
+          <span className="cursor-pointer text-blue-500" onClick={toggleSignIn}>
+            {isSignIn ? " Sign Up" : "Sign In"}
+          </span>
+          {isSignIn ? " Now" : ""}
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Login;

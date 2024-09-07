c0c4d74a5c9941aee556f501d3831f2b60b3a05b
Ridhima <22b01a12b6@svecw.edu.in>
2024-09-07 18:47:54 +0530

    added form validation

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -1,8 +1,14 @@
 const Validate=(email,password)=>{
     const isEmailValid = /[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}/.test(email);
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    if(!isEmailValid) return "Email,password is/are not valid";
-    if(!isPasswordValid) return "Email,password is/are not valid";
+    if(!isEmailValid)
+        {
+             return "Email,password is/are not valid";
+        }
+    if(!isPasswordValid) 
+        {
+            return "Email,password is/are not valid";
+        }
     return null;
 } 
 export default Validate;

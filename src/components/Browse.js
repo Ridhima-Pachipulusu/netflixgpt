6f613238c78e5d0a5cad99b4825135d6dd32bcf4
Ridhima <22b01a12b6@svecw.edu.in>
2024-09-12 18:14:35 +0530

    movies data is fetched

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,8 +1,23 @@
+import { useEffect } from "react";
+import { Options } from "../utils/Constants";
 import Header from "./Header";
 
-const Browse=()=>{
-    return(
-        <div><Header/></div>
-    )
-}
+const Browse = () => {
+  const getMoviesList = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      Options
+    );
+    const json = await data.json();
+    console.log(json.results);
+  };
+  useEffect(() => {
+    getMoviesList();
+  }, []);
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 export default Browse;
---
+++ src/utils/Constants.js
@@ -0,0 +1,7 @@
+export const Options= {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzN2E2ZjQ2MDQ4NzQwYmFkZGM2N2M2NDFmMzQ2ZjI4ZCIsIm5iZiI6MTcyNjE0MzIzNC41MzYxLCJzdWIiOiI2NmUyZDk4MDAwMDAwMDAwMDBiOTIzOTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-nZTN9Q3bFcxYjfPZMn89Vhou274ffVFiDm1G2SrZlk'
+  }
+};
